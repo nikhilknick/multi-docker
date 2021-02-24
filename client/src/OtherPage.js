7d6d14fb2bdb38236f48ba1ab534a3364d75d3ca
Nikhil Arya <nikhilknick@gmail.com>
2021-02-24 19:20:53 +0530

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      This is other page
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
